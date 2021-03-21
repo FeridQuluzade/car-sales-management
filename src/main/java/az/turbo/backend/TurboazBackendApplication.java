284e0a6288511e7d90b3e21a450839ad298ec5f2
Jeyhun Gasimov <jeyhun.gasimov@atltech.az>
2021-03-21 09:49:47 +0400

    Init project

---
+++ src/main/java/az/turbo/backend/TurboazBackendApplication.java
@@ -0,0 +1,13 @@
+package az.turbo.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TurboazBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TurboazBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/az/turbo/backend/TurboazBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package az.turbo.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TurboazBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
