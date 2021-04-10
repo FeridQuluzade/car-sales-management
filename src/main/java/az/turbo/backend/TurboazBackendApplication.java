288dff1572a1fb5bbdbdd79ccd2954e161ae5a3c
Jeyhun Gasimov <jeyhun.gasimov@atltech.az>
2021-04-10 18:45:39 +0400

    Fix some confilicts

--- src/main/java/az/turbo/backend/TurboazBackendApplication.java
+++ src/main/java/az/turbo/backend/TurboazBackendApplication.java
@@ -1,29 +1,11 @@
 package az.turbo.backend;
 
-import az.turbo.backend.bodytypes.application.BodyTypeService;
-import az.turbo.backend.bodytypes.application.BodyTypeServiceImpl;
-import az.turbo.backend.bodytypes.application.dto.BodyCreateDto;
-import az.turbo.backend.bodytypes.application.dto.BodyTypeUpdateDto;
-import az.turbo.backend.bodytypes.application.exception.BodyNotFoundException;
 import az.turbo.backend.colors.application.ColorService;
 import az.turbo.backend.colors.application.ColorServiceImpl;
 import az.turbo.backend.colors.application.dto.ColorCreateDto;
-import az.turbo.backend.users.application.UserService;
-import az.turbo.backend.users.application.UserServiceImpl;
-import az.turbo.backend.users.application.dto.UserCreateDto;
-import az.turbo.backend.users.application.dto.UserDto;
-import az.turbo.backend.users.application.dto.UserUpdateDto;
-import az.turbo.backend.users.domain.model.Gender;
-import az.turbo.backend.users.domain.model.User;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.thymeleaf.expression.Sets;
 
-import java.sql.*;
 import java.time.LocalDateTime;
-import java.util.Arrays;
-import java.util.List;
-import java.util.Set;
-import java.util.stream.Collectors;
 
 @SpringBootApplication
 public class TurboazBackendApplication {
