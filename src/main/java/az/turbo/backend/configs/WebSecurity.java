7f9f2639e2f9e954c7c94017314bb4862e99bdd7
Yunus <yunus.kazimov@gmail.com>
2021-04-16 14:31:51 +0400

    Turbo.az-35: write cities and customers HttpMethods on WebSecurity

--- src/main/java/az/turbo/backend/configs/WebSecurity.java
+++ src/main/java/az/turbo/backend/configs/WebSecurity.java
@@ -50,6 +50,20 @@ class WebSecurity extends WebSecurityConfigurerAdapter {
                 .antMatchers(HttpMethod.PUT, "/body-types/update").permitAll()
                 .antMatchers(HttpMethod.DELETE, "/body-types/delete/**").permitAll()
 
+                //cities
+                .antMatchers(HttpMethod.GET, "/cities/retrieve-all").permitAll()
+                .antMatchers(HttpMethod.GET, "/cities/retrieve-by-id/**").permitAll()
+                .antMatchers(HttpMethod.POST, "/cities/create").permitAll()
+                .antMatchers(HttpMethod.PUT, "/cities/update").permitAll()
+                .antMatchers(HttpMethod.DELETE, "/cities/delete/**").permitAll()
+
+                //customers
+                .antMatchers(HttpMethod.GET, "/customers/retrieve-all").permitAll()
+                .antMatchers(HttpMethod.GET, "/customers/retrieve-by-id/**").permitAll()
+                .antMatchers(HttpMethod.POST, "/customers/create").permitAll()
+                .antMatchers(HttpMethod.PUT, "/customers/update").permitAll()
+                .antMatchers(HttpMethod.DELETE, "/customers/delete/**").permitAll()
+
                 //PRIVATE ENDPOINTS
                 .anyRequest()
                 .authenticated()
