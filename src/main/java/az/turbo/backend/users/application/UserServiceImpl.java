e18d58af169637bfe3708a3d5f53fc00a90b60f4
Jeyhun Gasimov <jeyhun.gasimov@atltech.az>
2021-04-18 10:12:27 +0400

    Merge branch 'master' of https://gitlab.com/atl_academy_group_anar/turbo.az-backend into Turbo.az-21
    # Conflicts:
    #	src/main/java/az/turbo/backend/TurboazBackendApplication.java
    #	src/main/java/az/turbo/backend/cities/application/CityService.java

--- src/main/java/az/turbo/backend/users/application/UserServiceImpl.java
+++ src/main/java/az/turbo/backend/users/application/UserServiceImpl.java
@@ -60,7 +60,6 @@ public class UserServiceImpl implements UserService {
         return userRepository.create(user);
     }
 
-    //Jalal
     @Override
     public Set<Long> createAll(List<UserCreateDto> userCreateDtoList) {
         return null;
