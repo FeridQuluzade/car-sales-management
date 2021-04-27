c1bc7d939e5d279890c8bc4ac3eeb5371e193812
quluzadef36 <quluzadef36@gmail.com>
2021-04-28 01:11:56 +0400

    HOTFIX:change setName as setValue and getName as getValue

--- src/main/java/az/turbo/backend/engineVolumes/application/dto/EngineVolumeCreateDto.java
+++ src/main/java/az/turbo/backend/engineVolumes/application/dto/EngineVolumeCreateDto.java
@@ -12,11 +12,11 @@ public class EngineVolumeCreateDto extends AuditedCreateDto {
         this.value = value;
     }
 
-    public String getName() {
+    public String getValue() {
         return value;
     }
 
-    public void setName(String value) {
+    public void setValue(String value) {
         this.value = value;
     }
 }
